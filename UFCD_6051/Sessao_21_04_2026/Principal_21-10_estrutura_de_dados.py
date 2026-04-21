65740001872000afa0c16d28cfff873aeec7c0da
Carlos Ribeiro <carlosribeiro-9@hotmail.com>
2026-04-21 22:52:35 +0100

    Add files via upload

---
+++ UFCD_6051/Sessao_21_04_2026/Principal_21-10_estrutura_de_dados.py
@@ -0,0 +1,59 @@
+#
+dia1 = 100
+dia2 = 40
+dia3 = 150
+dia4 = 60
+dia5 = 80
+
+# Método 1
+# estrutura de dados -> lista
+dia = [
+    100,
+    40,
+    150,
+    60,
+    80
+]
+print(dia)
+
+# Método 2
+# estrutura de dados -> mapeamento -> dicionario
+formando = {
+    "id": "111111110xz0",
+    "nome": "Bruno",
+    "idade": 20,
+    "cidade": ["Felgueiras", "Vizela"],
+    "nif": "111111111",
+    "nacionalidade": [
+        "portuguesa",
+        "espanhola"
+    ]
+}
+
+formandos = [
+    {
+    "id": "111111110xz0",
+    "nome": "Bruno",
+    "idade": 20,
+    "cidade": ["Felgueiras", "Vizela"],
+    "nif": "111111111",
+    "nacionalidade": [
+        "portuguesa",
+        "espanhola"
+    ]
+},
+    {
+    "id": "222222220xz0",
+    "nome": "Carlos",
+    "idade": 30,
+    "cidade": "Vizela",
+    "nif": "222222222"
+},
+{
+    "id": "333333330xz0",
+    "nome": "Daniel",
+    "idade": 29,
+    "cidade": "Vizela",
+    "nif": "333333333"
+}
+]
